f1c0fb62397582fb00d5288950af676b766f0f3a
Ivan Satsiuk <ivan.satsiuk@work.com>
2024-05-09 15:03:06 +0200

    update mappings

--- lua/user/core/keymaps.lua
+++ lua/user/core/keymaps.lua
@@ -2,27 +2,27 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
-keymap.set("n", "<leader>w", vim.cmd.write, {desc = "Save file"})
-keymap.set("n", "<leader>+", "<C-a>", {desc = "Increment a number"})
-keymap.set("n", "<leader>-", "<C-x>", {desc = "Decrement a number"})
+keymap.set("n", "<leader>w", vim.cmd.write, { desc = "Save file" })
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment a number" })
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement a number" })
 
 -- splits
 -- keymap.set("n", "<leader>s=", "<C-w>=", {desc = "Make splits equal width"})
-keymap.set("n", "<leader>x", "<cmd>close<cr>", {desc = "Close current split"})
+keymap.set("n", "<leader>x", "<cmd>close<cr>", { desc = "Close current split" })
 
 -- buffers
-keymap.set("n", "H", "<cmd>bprev<cr>", {desc = "Prev open file"})
-keymap.set("n", "L", "<cmd>bnext<cr>", {desc = "Next open file"})
+keymap.set("n", "H", "<cmd>bprev<cr>", { desc = "Prev open file" })
+keymap.set("n", "L", "<cmd>bnext<cr>", { desc = "Next open file" })
 
 -- exit
-keymap.set("n", "<leader>q",vim.cmd.quitall, {desc = "Quit"})
-keymap.set("n", "<C-w>", vim.cmd.quit, {desc = "Close file"})
-keymap.set("n", "<leader>c",vim.cmd.bdelete, {desc = "Quit"})
+keymap.set("n", "<leader>q", vim.cmd.quitall, { desc = "Quit" })
+keymap.set("n", "<C-w>", vim.cmd.quit, { desc = "Close file" })
+keymap.set("n", "<leader>c", vim.cmd.bdelete, { desc = "Quit" })
 
-vim.keymap.set("n", "<leader>wso", ':w<cr>:so<cr>', {desc = "Write and source"})
+vim.keymap.set("n", "<leader>wso", ":w<cr>:so<cr>", { desc = "Write and source" })
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", {desc = "Move Line in Visual mode"})
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv",{desc = "Move Line in Visual mode"})
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move Line in Visual mode" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move Line in Visual mode" })
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -34,10 +34,10 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
@@ -48,12 +48,28 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 -- vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 -- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-vim.keymap.set("n", "<leader>u", vim.cmd.nohlsearch, {desc = "Un-hightlight"})
+vim.keymap.set("n", "<leader>u", vim.cmd.nohlsearch, { desc = "Un-hightlight" })
 
 -- redo
 vim.keymap.set("n", "U", vim.cmd.redo)
 
--- diagnostics navigatin
-vim.keymap.set("n","[d", vim.diagnostic.goto_prev)
-vim.keymap.set("n","]d", vim.diagnostic.goto_next)
-vim.keymap.set("n","<leader>d", vim.diagnostic.open_float)
+-- diagnostics navigation
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
+
+-- handy dandy
+vim.keymap.set("n", "Y", "Y")
+vim.keymap.set("n", "|", ":vs<cr>")
+vim.keymap.set("n", "cw", "ciw")
+vim.keymap.set("n", "yw", "yiw")
+vim.keymap.set("n", "dw", "diw")
+vim.keymap.set("n", "cW", "ciW")
+vim.keymap.set("n", "yW", "yiW")
+vim.keymap.set("n", "dW", "diW")
+
+-- window navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
--- lua/user/plugins/fzf-lua.lua
+++ lua/user/plugins/fzf-lua.lua
@@ -1,32 +1,39 @@
 return {
-  "ibhagwan/fzf-lua",
-  -- optional for icon support
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    -- calling `setup` is optional for customization
-    local fzf = require("fzf-lua")
-    fzf.setup({
-      winopts = {
-        width = 0.9,
-        column = 0.45,
-        row = 0.25,
-        height = 0.9,
-        preview = {
-          delay = 300,
-        },
-        lsp = {
-          jump_to_single_result = true
-        },
-      },
-    })
-    vim.keymap.set("n", "<leader>f", ":FzfLua files<cr>", { desc = "Find file" })
-    vim.keymap.set("n", "<leader>sl", ":FzfLua resume<cr>", { desc = "Open last search" })
-    vim.keymap.set("n", "<leader>sr", ":FzfLua oldfiles<cr>", { desc = "Search recent" })
-    vim.keymap.set("n", "<leader>ss", ":FzfLua live_grep<cr>", { desc = "Search word" })
-    vim.keymap.set("n", "<leader>se", ":FzfLua grep_cword<cr>", { desc = "Search word under cursor" })
-    vim.keymap.set("v", "<leader>se", ":FzfLua grep_visual<cr>", { desc = "Search selcted text" })
-    vim.keymap.set("n", "<leader>sv", ":FzfLua lsp_document_symbols<cr>", { desc = "Search variables" })
-    vim.keymap.set("n", "gD", function() fzf.lsp_declarations() end, { desc = "Go to declaration" })
-    vim.keymap.set("n", "gd", function() fzf.lsp_definitions({jump_to_single_result = true}) end, { desc = "Show buffer diagnostics" })
-  end
+	"ibhagwan/fzf-lua",
+	-- optional for icon support
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		-- calling `setup` is optional for customization
+		local fzf = require("fzf-lua")
+		fzf.setup({
+			winopts = {
+				width = 0.9,
+				column = 0.45,
+				row = 0.25,
+				height = 0.9,
+				preview = {
+					delay = 300,
+				},
+				lsp = {
+					jump_to_single_result = true,
+				},
+			},
+		})
+		vim.keymap.set("n", "<leader>f", ":FzfLua files<cr>", { desc = "Find file" })
+		vim.keymap.set("n", "<leader>sl", ":FzfLua resume<cr>", { desc = "Open last search" })
+		vim.keymap.set("n", "<leader>sr", ":FzfLua oldfiles<cr>", { desc = "Search recent" })
+		vim.keymap.set("n", "<leader>ss", ":FzfLua live_grep<cr>", { desc = "Search word" })
+		vim.keymap.set("n", "<leader>se", ":FzfLua grep_cword<cr>", { desc = "Search word under cursor" })
+		vim.keymap.set("v", "<leader>se", ":FzfLua grep_visual<cr>", { desc = "Search selcted text" })
+		vim.keymap.set("n", "<leader>sv", ":FzfLua lsp_document_symbols<cr>", { desc = "Search variables" })
+		vim.keymap.set("n", "gD", function()
+			fzf.lsp_declarations()
+		end, { desc = "Go to declaration" })
+		vim.keymap.set("n", "gd", function()
+			fzf.lsp_definitions({ jump_to_single_result = true })
+		end, { desc = "Go to definition" })
+		vim.keymap.set("n", "gv", function()
+			fzf.lsp_definitions({ jump_to_single_result = true, jump_to_single_result_action = fzf.actions.file_vsplit })
+		end, { desc = "Go to definition in split" })
+	end,
 }
