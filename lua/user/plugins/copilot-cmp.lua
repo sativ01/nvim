32b46ccb6a80e6b0a662f0e9ab3f851e59124b54
Ivan Satsiuk <ivan.satsiuk@work.com>
2024-12-15 18:13:57 +0100

    copilot

--- lua/user/plugins/copilot-cmp.lua
+++ lua/user/plugins/copilot-cmp.lua
@@ -1,13 +1,13 @@
 return {
-	"zbirenbaum/copilot-cmp",
-	event = "InsertEnter",
-	dependencies = { "zbirenbaum/copilot.lua" },
-	config = function()
-		vim.defer_fn(function()
-			require("copilot").setup({
-				copilot_node_command = vim.fn.expand("$HOME") .. "/.nvm/versions/node/v18.19.0/bin/node", -- Node.js version must be > 18.x
-			}) -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
-			require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
-		end, 100)
-	end,
+	-- "zbirenbaum/copilot-cmp",
+	-- event = "InsertEnter",
+	-- dependencies = { "zbirenbaum/copilot.lua" },
+	-- config = function()
+	-- 	vim.defer_fn(function()
+	-- 		require("copilot").setup({
+	-- 			copilot_node_command = vim.fn.expand("$HOME") .. "/.nvm/versions/node/v18.19.0/bin/node", -- Node.js version must be > 18.x
+	-- 		}) -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+	-- 		require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+	-- 	end, 100)
+	-- end,
 }
--- lua/user/plugins/gitsigns.lua
+++ lua/user/plugins/gitsigns.lua
@@ -20,8 +20,8 @@ return {
       -- stylua: ignore start
       map("n", "<leader>gj", gs.next_hunk, "Next Hunk")
       map("n", "<leader>gk", gs.prev_hunk, "Prev Hunk")
-      map({ "n", "v" }, "gs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
-      -- map({ "n", "v" }, "gr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+      map({ "n", "v" }, "<leader>gs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+      map({ "n", "v" }, "<leader>gr", ":Gitsigns reset_hunk<CR>", "Git Undo")
       map("n", "<leader>gS", gs.stage_buffer, "Stage Buffer")
       map("n", "<leader>gu", gs.undo_stage_hunk, "Undo Stage Hunk")
       map("n", "<leader>gR", gs.reset_buffer, "Reset Buffer")
